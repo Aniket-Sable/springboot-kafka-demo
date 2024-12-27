95e7b30dcd6eadbab8d04c9819bbf40631a81d1a
[Aniket-Sable] <aniketsablepatil99k@gmail.com>
2024-12-27 19:07:18 +0530

    Initial commit

---
+++ consumer/consumer/src/main/java/com/kafka/consumer/consumer/config/AppConstant.java
@@ -0,0 +1,7 @@
+package com.kafka.consumer.consumer.config;
+
+public class AppConstant {
+
+    public static final String LOCATION_UPDATE_TOPIC = "location-update-topic";
+    public final static String GROUP_ID="group-1";
+}
